3f6a9dcea2d883a0691510f03d354186d3d7907d
Haochuan Liu <haochuan.liu@gmail.com>
2015-02-25 23:26:57 -0800

    info

--- public/js/script.js
+++ public/js/script.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
     $(document).foundation();
 
     /**
-    *
+    * I have changed some thing here
     * $('#red') --- select the HTML element which has the id: red
-    * 
+    *
     * $('#red').on('change.fndtn.slider', function())
-    * 
+    *
     * Add an event listener on $('#red'), the event it listens to is: change.fndtn.slider
-    * which means when change the slider in the webpage. For more detail about Foundation 
+    * which means when change the slider in the webpage. For more detail about Foundation
     * event, please see: http://foundation.zurb.com/docs/
     *
     * socket.emit('red', { val: $('#red').attr('data-slider')})
@@ -19,7 +19,7 @@ $(document).ready(function() {
     * first parameter is the event name, second is the data.
     *
     * See README.md for more details about how to communicate between front end and back end
-    * 
+    *
     **/
 
     $('#red').on('change.fndtn.slider', function(){
